5ada743f9d235e091de508f217b0a37176a53bca
Drew Thompson <drewallynthompson@gmail.com>
2020-10-12 00:11:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,7 +21,7 @@ class Application
         @@cart.each {|item| resp.write "#{item}\n"}
       end
     elsif req.path.match(/add/)
-      
+      search_term = req.params['']
     else
       resp.write "Path Not Found"
     end
